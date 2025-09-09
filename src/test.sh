f1dc2231c281187b19debd102c9bce4bce19f943
emad.alowayb <emad.alowayb@zaho.ly>
2025-09-09 12:46:40 +0200

    Fix test.sh syntax

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
-#src/test.sh
-EXPECTED="Hello World"
-OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./app')('Test'))")
 
 if [[ "$OUTPUT" == "$EXPECTED" ]]; then
     echo "Test passed"
